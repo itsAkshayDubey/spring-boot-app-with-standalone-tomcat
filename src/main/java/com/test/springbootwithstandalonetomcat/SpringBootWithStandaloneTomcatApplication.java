2b607ec4ea14e7280fbc8c2e3aaab271e2d6eeca
Akshay Dubey <aksdubey@publicisgroupe.net>
2022-06-06 12:41:34 +0530

    Initial commit

---
+++ src/main/java/com/test/springbootwithstandalonetomcat/SpringBootWithStandaloneTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.test.springbootwithstandalonetomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithStandaloneTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithStandaloneTomcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/springbootwithstandalonetomcat/SpringBootWithStandaloneTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.springbootwithstandalonetomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWithStandaloneTomcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
