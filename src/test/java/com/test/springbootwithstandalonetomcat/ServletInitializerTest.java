084d695140ca8634e1096ffacfdbf1eb7d7518d5
Akshay Dubey <akshay.dubey055@gmail.com>
2022-08-01 22:45:36 +0530

    Add unit tests

---
+++ src/test/java/com/test/springbootwithstandalonetomcat/ServletInitializerTest.java
@@ -0,0 +1,14 @@
+package com.test.springbootwithstandalonetomcat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ServletInitializerTest {
+
+	@Test
+	void testConfigureSpringApplicationBuilder() {
+		assertTrue(true);
+	}
+
+}
---
+++ src/test/java/com/test/springbootwithstandalonetomcat/SpringBootWithStandaloneTomcatApplicationTest.java
@@ -0,0 +1,19 @@
+package com.test.springbootwithstandalonetomcat;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SpringBootWithStandaloneTomcatApplicationTest {
+
+	@Test
+	void testMain() {
+		assertTrue(true);
+	}
+
+	@Test
+	void testRunStringArray() {
+		assertTrue(true);
+	}
+
+}
