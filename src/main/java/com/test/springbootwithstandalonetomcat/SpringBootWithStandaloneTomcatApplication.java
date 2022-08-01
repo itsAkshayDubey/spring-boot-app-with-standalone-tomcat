9c99477d79476d42f6bd288f2bc7db66645c667a
Akshay Dubey <aksdubey@publicisgroupe.net>
2022-08-01 19:15:43 +0530

    Add run method to main class

--- src/main/java/com/test/springbootwithstandalonetomcat/SpringBootWithStandaloneTomcatApplication.java
+++ src/main/java/com/test/springbootwithstandalonetomcat/SpringBootWithStandaloneTomcatApplication.java
@@ -1,13 +1,22 @@
 package com.test.springbootwithstandalonetomcat;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWithStandaloneTomcatApplication {
-
+public class SpringBootWithStandaloneTomcatApplication extends SpringBootServletInitializer implements CommandLineRunner{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithStandaloneTomcatApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println(">>>>>>>>>> Hello World <<<<<<<<<<");
+				
 	}
 
 }
